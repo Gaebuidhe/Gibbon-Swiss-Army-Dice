f0d13bec9e11d5bc0d09d496bee0f9380b4d1c79
Joe Frazier <faolannus@gmail.com>
2018-06-04 14:30:24 -0400

    Initial Mechanics Set up
    DND mechanics file set up

---
+++ Gibbon-Swiss-Army-Dice/Gibbon-Swiss-Army-Dice/DNDmechanics.swift
@@ -0,0 +1,26 @@
+//
+//  DNDmechanics.swift
+//  Gibbon-Swiss-Army-Dice
+//
+//  Created by Joseph Frazier on 6/4/18.
+//  Copyright © 2018 Joseph Frazier. All rights reserved.
+//
+
+import UIKit
+
+class DNDmechanics{
+    var roll: Int = 0
+    var diffCheck: Int = 0
+    var bonus: Int = 0
+    var advantage: Bool = false
+    var disadvantage: Bool = false
+    
+    static func adndAttack(nroll: Int, ndiff: Int) -> Bool{
+        
+        
+        
+        return false
+    }
+    
+    
+}
