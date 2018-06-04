d02e889b820d1f25e0c99ee3669c4a47f16adcb1
Joe Frazier <faolannus@gmail.com>
2018-06-04 14:04:48 -0400

    Setup2

---
+++ Gibbon-Swiss-Army-Dice/Gibbon-Swiss-Army-DiceUITests/Gibbon_Swiss_Army_DiceUITests.swift
@@ -0,0 +1,36 @@
+//
+//  Gibbon_Swiss_Army_DiceUITests.swift
+//  Gibbon-Swiss-Army-DiceUITests
+//
+//  Created by Joseph Frazier on 6/4/18.
+//  Copyright © 2018 Joseph Frazier. All rights reserved.
+//
+
+import XCTest
+
+class Gibbon_Swiss_Army_DiceUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
