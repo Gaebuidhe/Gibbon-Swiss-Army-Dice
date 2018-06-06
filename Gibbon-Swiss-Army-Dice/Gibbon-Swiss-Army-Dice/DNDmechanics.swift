d86ffd43cc156bf9ddb9a2a7dfd041b0a23f9192
Joe Frazier <faolannus@gmail.com>
2018-06-06 14:11:52 -0400

    mechanics update
    function added (adnd Attack roll)

--- Gibbon-Swiss-Army-Dice/Gibbon-Swiss-Army-Dice/DNDmechanics.swift
+++ Gibbon-Swiss-Army-Dice/Gibbon-Swiss-Army-Dice/DNDmechanics.swift
@@ -9,17 +9,42 @@
 import UIKit
 
 class DNDmechanics{
-    var roll: Int = 0
-    var diffCheck: Int = 0
-    var bonus: Int = 0
-    var advantage: Bool = false
-    var disadvantage: Bool = false
     
-    static func adndAttack(nroll: Int, ndiff: Int) -> Bool{
+    static func adndAttack(nroll: Int, nthaco: Int) -> String{
+        //sets up return variable
+        var hitDC: String = ""
+        
+        //creates
+        let roll: Int = nroll
+        let thaco: Int = nthaco
+        let ac = thaco - roll
         
         
+        //simple mechanics first 20's hit 1's miss
+        //followed by roll/thaco comparison
+        if(nroll == 20){
+            hitDC = "You Hit!"
+        }
+        else if (nroll == 1){
+            hitDC = "You Missed!"
+        }
+        else {
+            hitDC = "You hit Armor Class " + String(ac) + "!"
+        }
         
-        return false
+        return hitDC
+    }
+    
+    static func dcCheck(nroll:Int, ndc:Int, nbonus:Int){
+        
+    }
+    
+    static func adndProf(nroll: Int, natb: Int, nbonus:Int)->String{
+        //sets up return variable
+        var profCheck: String = ""
+        
+        
+        return profCheck
     }
     
     
